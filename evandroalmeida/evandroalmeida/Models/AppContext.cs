3615e792314b78154a7ed5e164fa7b28941443a7
evandrofa <evandro.almeida@live.com>
2016-06-21 21:44:25 -0300

    Trabalho PI

---
+++ evandroalmeida/evandroalmeida/Models/AppContext.cs
@@ -0,0 +1,16 @@
+﻿using System.Data.Entity;
+
+namespace evandroalmeida.Models
+{
+    public class AppContext : DbContext
+    {
+        public AppContext() : base("name=AppContext")
+        {
+        }
+
+        public System.Data.Entity.DbSet<Genero> Generos { get; set; }
+        public System.Data.Entity.DbSet<Autor> Autores { get; set; }
+        public System.Data.Entity.DbSet<Livro>Livros { get; set; }
+    }
+    
+}
